9cf59be72b7c3edb14282d62b3ce35e98802d786
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-02-27 11:39:38 +0000

    browser use init

---
+++ browser.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=ChatOpenAI(model="gpt-4o"),
+    )
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
